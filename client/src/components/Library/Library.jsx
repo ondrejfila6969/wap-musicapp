e72e053199c4f10d040acec52ea1a09603cb5b4a
tade18 <tadeaskeller@gmail.com>
2025-05-20 11:14:38 +0200

    library custom scrollbar

--- client/src/components/Library/Library.jsx
+++ client/src/components/Library/Library.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import LibraryHeader from '../LibraryHeader/LibraryHeader'
 import LibraryPlaylist from '../LibraryPlaylist/LibraryPlaylist'
+import "./Library.css";
 
 export default function Library() {
   return (
     <div className="flex-1 bg-gray-400 rounded-3xl sonus-bg-linear-gradient p-8 flex flex-col">
       <LibraryHeader />
       
-      <div className="mt-5 overflow-y-scroll flex-1 pr-2">
+      <div className="mt-5 overflow-y-scroll flex-1 pr-2 library-wrapper">
         <div className="grid h-[200px] md:grid-cols-2 grid-cols-1 gap-4">
           <LibraryPlaylist imageUrl={"playlistCoverExample.png"} title={"Playlist Example 1"} />
           <LibraryPlaylist imageUrl={"playlistCoverExample.png"} title={"Playlist Example 2"} />
