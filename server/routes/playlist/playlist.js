d70e2bcca2504737f03281118990d9b6ab367fc7
Michalovronka <kokosnahlave123@seznam.cz>
2025-05-20 10:39:22 +0200

    pleasse

--- server/routes/playlist/playlist.js
+++ server/routes/playlist/playlist.js
@@ -4,7 +4,7 @@ const playlistControllers = require("../../controllers/playlist/playlist");
 
 router.get('/', playlistControllers.getAllPlaylists);
 router.get('/:id', playlistControllers.getPlaylistById);
-router.post('/', playlistControllers.createPlaylist);
+router.post('/:userId', playlistControllers.createPlaylist); //create playlist pak pro kazdou songu v playlistu pushni do arraye na id alba ziskanej z frontedu
 router.put('/:id', playlistControllers.updatePlaylist);
 router.delete('/:id', playlistControllers.deletePlaylist);
 
