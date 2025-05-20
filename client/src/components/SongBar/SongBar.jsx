d62a0b4226ef52cb436c19d00c77aa2e206d26a1
FeeXtt <stanislav.cinklstudent.spsmb.cz>
2025-05-20 10:55:53 +0200

    songbar

---
+++ client/src/components/SongBar/SongBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { PlayIcon } from "@heroicons/react/24/solid";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+
+
+import { useState } from "react";
+
+export default function SongBar() {
+  const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const songNumber = 1;
+  return (
+        <div
+      className="flex items-center justify-between px-4 py-2 hover:bg-neutral-800 transition-colors text-white text-sm"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <div className="flex items-center gap-4">
+        <div className="w-4 h-4 flex items-center justify-center text-white">
+          {hovered ? <PlayIcon className="w-4 h-4 cursor-pointer" /> : <span>{songNumber}</span>}
+        </div>
+        <div>
+          <div className="font-semibold">idk</div>
+          <div className="text-neutral-400 text-xs">idk</div>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 text-neutral-400 text-xs ">
+        {hovered && <PlusCircleIcon className="w-4 h-4 text-white cursor-pointer mr-2" />}
+        <span>3:20</span>
+      </div>
+    </div>
+
+  );
+}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import SongBar from "../../components/SongBar/SongBar";
+
 
 export default function Home() {
   return (
     <div className="text-white p-8">
-      <div className="text-3xl">Home</div>
+      <div className="text-3xl">
+        <SongBar />
+        <SongBar />
+        <SongBar />
+        <SongBar />
+        <SongBar />
+        <SongBar />
+      </div>
     </div>
   );
 }
