fc2c0bcfeaa10e9e9ffb780951a7518518316355
ondrejfila6969 <ofila12@seznam.cz>
2025-05-27 08:28:00 +0200

    User auth done

--- client/src/components/RegisterForm/RegisterForm.jsx
+++ client/src/components/RegisterForm/RegisterForm.jsx
@@ -1,19 +1,64 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../../context/AuthProvider";
+import { registerUser } from "../../models/user";
 
 export default function RegisterForm() {
+  const [formData, setFormData] = useState()
+  const [info, setInfo] = useState();
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const sendData = async () => {
+    const emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    if (!emailRegex.test(formData.email)) return setInfo("Invalid email format");
+
+    const res = await registerUser(formData);
+    if (res.status === 201) {
+      login(res.token);
+      alert("REGISTER SUCCESS !");
+      return navigate("/");
+    }
+    setInfo(res.message);
+  }
+
+  const handleInput = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const handleButton = (e) => {
+    e.preventDefault();
+    sendData();
+  };
+
   return (
     <div className="flex justify-left items-center sm:w-1/2 w-full px-4 pb-12 sm:pb-0">
       <div className="sonus-bg-linear-gradient bg-opacity-50 p-8 rounded-3xl shadow-md w-full max-w-md">
         <form className="flex flex-col gap-4">
           <div>
             <label htmlFor="username" className="block mb-1 text-sm">
-              Username
+              First name
             </label>
             <input
-              id="username"
               type="text"
+              name="first_name"
               className="w-full px-4 py-2 bg-[#2e2e2e] rounded-full focus:outline-none text-white"
+              required onChange={handleInput}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="username" className="block mb-1 text-sm">
+              Last name
+            </label>
+            <input
+              type="text"
+              name="last_name"
+              className="w-full px-4 py-2 bg-[#2e2e2e] rounded-full focus:outline-none text-white"
+              required onChange={handleInput}
             />
           </div>
 
@@ -22,9 +67,10 @@ export default function RegisterForm() {
               Email
             </label>
             <input
-              id="id"
               type="email"
+              name="email"
               className="w-full px-4 py-2 bg-[#2e2e2e] rounded-full focus:outline-none text-white"
+              required onChange={handleInput}
             />
           </div>
 
@@ -33,26 +79,17 @@ export default function RegisterForm() {
               Password
             </label>
             <input
-              id="password"
               type="password"
+              name="password"
               className="w-full px-4 py-2 bg-[#2e2e2e] rounded-full focus:outline-none text-white"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password" className="block mb-1 text-sm">
-              Password Again
-            </label>
-            <input
-              id="passwordVer"
-              type="password"
-              className="w-full px-4 py-2 bg-[#2e2e2e] rounded-full focus:outline-none text-white"
+              required onChange={handleInput}
             />
           </div>
 
           <button
             type="submit"
             className="bg-gradient-to-r from-gray-900 to-green-900 rounded-full py-2 font-semibold text-white hover:opacity-90 transition cursor-pointer"
+            onClick={handleButton}
           >
             Register
           </button>
